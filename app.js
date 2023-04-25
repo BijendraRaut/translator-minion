86b1b293d52801cfedfceb8c8fe85b6b5d328668
Bijendra Raut <bijendraghvm@gmail.com>
2023-04-25 18:34:27 +0530

    translation wrk done

--- app.js
+++ app.js
@@ -2,8 +2,17 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var txtOutput = document.querySelector("#txt-output");
 
-btnTranslate.addEventListener("click", function () {
+var url = "https://api.funtranslations.com/translate/minion.json?text=";
+
+function errorHandler() {
+  console.log("error occured", error);
+  alert("something wrong with server! try after sometime");
+}
+
+btnTranslate.addEventListener("click", function fetchData() {
   var inputValue = txtInput.value;
-  txtOutput.innerHTML =
-    "fsssss fsfsfsfsf sfsfsf" + "this is the translated one of - " + inputValue;
+  fetch(url + inputValue)
+    .then((res) => res.json())
+    .then((json) => (txtOutput.innerText = json.contents.translated))
+    .catch(errorHandler);
 });
